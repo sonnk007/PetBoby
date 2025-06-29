7724e6c0764c7fb05c2e2266c12d1b1466c4aec9
sonnk007 <kson8504@gmail.com>
2025-06-29 16:44:20 +0700

    add new project

---
+++ petproject/src/main/java/com/sonnk/petproject/PetprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sonnk.petproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetprojectApplication.class, args);
+	}
+
+}
