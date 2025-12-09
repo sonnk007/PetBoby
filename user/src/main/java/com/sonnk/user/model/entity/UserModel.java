39b3c80f99a3c8e485b9e85d46abd4d19f41794e
sonnk <sonnk@chainosglobal.com>
2025-12-10 01:57:02 +0700

    refactor code

---
+++ user/src/main/java/com/sonnk/user/model/entity/UserModel.java
@@ -0,0 +1,7 @@
+package com.sonnk.user.model.entity;
+
+import com.sonnk.user.model.entity.base.BaseEntity;
+
+public class UserModel extends BaseEntity {
+
+}
---
+++ user/src/main/java/com/sonnk/user/model/entity/base/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.sonnk.user.model.entity.base;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity {
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    private String deletedBy;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void markDeleted(String user) {
+        this.deletedAt = LocalDateTime.now();
+        this.deletedBy = user;
+    }
+}
